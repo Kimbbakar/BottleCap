63c56e7bdf5a615a4f040f2bf236b8f4b176c331
Kimbbakar <chowdhuryosman04@gmail.com>
2018-08-26 23:55:13 +0600

    update file name

--- script.bat
+++ script.bat
@@ -1,4 +1,4 @@
-DEL gxx.exe, lex.yy.c, y.output, y.tab.h, y.tab.c
-flex gxx.l
-bison -dyv gxx.y
-gcc lex.yy.c y.tab.c -o gxx.exe
+DEL BottleCap.exe, lex.yy.c, y.output, y.tab.h, y.tab.c
+flex BottleCap.l
+bison -dyv BottleCap.y
+gcc lex.yy.c y.tab.c -o BottleCap.exe
